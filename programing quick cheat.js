59267545a72fff4cbaa4419c36317df8ba966b0a
miguelpancada <103136534+miguelpancada@users.noreply.github.com>
2022-04-11 08:42:17 +0100

    Update programing quick cheat.js

--- programing quick cheat.js
+++ programing quick cheat.js
@@ -9,7 +9,7 @@
 	[1, "A", [1,2], {a: 1, b: "b"}] // Array
 	{a: 1, b: "b", c: [1,2], d: {a: 1, b:"b"}} // Object/Hash/Dictionary
 	// String
-		.slice(beginIndex); // retorna uma nova string a começar do beginIndex até ao fim da strin goriginal
+		.slice(beginIndex); // retorna uma nova string a começar do beginIndex até ao fim da string original
 		.slice(beginIndex, endIndex); // retorna uma nova string que começa do beginIndex e acaba no endIndex
 	// Array functions
 		.length // propriedade que retorna o tamanho do array
@@ -20,6 +20,8 @@
 		.indexOf(param1) // procura se o elemento existe no array, se sim retorna a posição do array, senaõ retorna -1
 		.splice(startIndex, totalMembers) // modifica o array original, começando no startIndex remove totalMembers e retorna um novo array com os elementos removidos 
 		.splice(startIndex, totalMembers, param1, param2,...) // same as above, mas ainda coloca o param1, param2,... dentro do array original a começar no startIndex
+		.slice(beginIndex); // retorna um novo array a começar do beginIndex até ao fim da string original
+		.slice(beginIndex, endIndex); // retorna um novo array que começa do beginIndex e acaba no endIndex
 	// Object/Hash/Dictionary functions
 		.hasOwnProperty(param1) // retorna true / false consoante o param1 existe / não existe como propriedade
 		delete {a: 1, b: "b"}[param1] // delete NÃO É uma function!! remove o par propriedade: valor 
